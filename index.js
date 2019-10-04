cef90233a91df81f1f26a7d24b3fdb10942342b5
Daniel <daniel@frytg.com>
2019-10-04 14:30:42 +0200

    updating design to match SWR audio lab

--- index.js
+++ index.js
@@ -12,21 +12,35 @@ const swaggerUi		= require('swagger-ui-express')
 const swaggerDocument	= require('./openapi.json')
 
 const customCss = '@import url(\'https://fonts.googleapis.com/css?family=Barlow+Semi+Condensed:300,400,500,600,700&display=swap\'); \n ' +
-	'label { display: inline; } ' +
+	'label { display: inline; padding: 0; } ' +
+	'.swagger-ui .servers>label select { margin: 0; } ' +
 	'td { padding: 9px 22px !important; } ' +
+	'td:hover { background: inherit; } ' +
 	'.swagger-ui .topbar { background: #003082; } ' +
 	'.swagger-ui .topbar img { display: none; }' +
 	'.swagger-ui .topbar .topbar-wrapper:after { content: \'SWR audio lab - Radiohub Documentation\'; padding: 15px 0; color: white; font-size: 25px; font-weight: 600; }' +
 	'.swagger-ui .topbar .download-url-wrapper { display: none } ' +
 	'.swagger-ui * { font-family: \'Barlow Semi Condensed\', -apple-system, sans-serif !important; }' +
 	'div.swagger-ui { max-width: 1080px; margin: 0 auto; } ' +
-	'div.swagger-ui .wrapper { padding: 15px; }' +
+	'div.swagger-ui .wrapper { padding: 15px; background: rgba(255,255,255,0.7) }' +
 	'div.swagger-ui .info .title small pre { padding: 2px 5px; }' +
 	'div.information-container.wrapper { margin: 6% 0 6% 0; padding: 15px; }' +
-	'div.swagger-ui .scheme-container { border-top: 1px solid #003082; margin: 0 0 6% 0; }' +
-	'div.swagger-ui .scheme-container .schemes { margin: 0; padding: 15px; border: 0; }' +
+	'div.swagger-ui .scheme-container { background: none; margin: 0 0 6% 0; padding: 0; box-shadow: none; }' +
+	'div.swagger-ui .scheme-container .schemes { margin: 0; padding: 15px; }' +
 	'div.swagger-ui section.models .model-container { background: rgba(255,255,255,0.6) !important; } ' +
-	'div.swagger-ui section.models .model-container:hover { background: white !important; } '
+	'div.swagger-ui section.models .model-container:hover { background: white !important; } ' +
+	'div.box, div.swagger-ui .wrapper { overflow: visible; } ' +
+	'div.swagger-ui .btn.authorize { background: rgb(255, 211, 53); border-color: transparent; color: black; } ' +
+	'div.swagger-ui .btn.authorize svg { fill: black; } ' +
+	'div.swagger-ui .btn.cancel { background: #ff6060; border-color: transparent; color: black; } ' +
+	'div.swagger-ui .opblock, div.swagger-ui .response-col_description__inner div.markdown, .swagger-ui .response-col_description__inner div.renderedMarkdown { border-radius: 0; box-shadow: none; } ' +
+	'div.swagger-ui .auth-container .wrapper { border: 0; } ' +
+	'div.swagger-ui .dialog-ux .modal-ux { border: 0; box-shadow: none; border-radius: 0; } ' +
+	'div.swagger-ui .opblock.opblock-get .opblock-summary-method { background: #003082; } ' +
+	'div.swagger-ui .opblock.opblock-get .opblock-summary { border-color: #003082; padding: 15px; } ' +
+	'div.swagger-ui .opblock.opblock-get { border-color: #003082; background: rgba(0, 148, 230, 0.03); } '
+
+
 
 const options = {
 	explorer:		true,
