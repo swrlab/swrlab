fbbe81e5028bbda22cf5260fd83d2716427816bb
Daniel <daniel@frytg.com>
2019-12-20 08:43:25 +0100

    removing g font, fixing style

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ const app		= express()
 const swaggerUi		= require('swagger-ui-express')
 const swaggerDocument	= require('./openapi.json')
 
-const customCss = '@import url(\'https://fonts.googleapis.com/css?family=Barlow+Semi+Condensed:300,400,500,600,700&display=swap\'); \n ' +
-	'label { display: inline; padding: 0; } ' +
+const customCss = 'label { display: inline; padding: 0; } ' +
 	'.swagger-ui .servers>label select { margin: 0; } ' +
+	'span.servers-title { display: none; }' +
+	'.swagger-ui select { font-weight: 400; }' +
 	'td { padding: 9px 22px !important; } ' +
 	'td:hover { background: inherit; } ' +
 	'tr:nth-child(even) { background: hsl(200, 0%, 96%); } ' +
@@ -21,13 +22,14 @@ const customCss = '@import url(\'https://fonts.googleapis.com/css?family=Barlow+
 	'.swagger-ui .topbar img { display: none; }' +
 	'.swagger-ui .topbar .topbar-wrapper:after { content: \'SWR audio lab - Radiohub Documentation\'; padding: 15px 0; color: white; font-size: 25px; font-weight: 600; }' +
 	'.swagger-ui .topbar .download-url-wrapper { display: none } ' +
-	'.swagger-ui * { font-family: \'Barlow Semi Condensed\', -apple-system, sans-serif !important; }' +
+	'.swagger-ui * { font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji" !important; }' +
 	'div.swagger-ui { max-width: 1080px; margin: 0 auto; } ' +
 	'div.swagger-ui .wrapper { padding: 15px; background: rgba(255,255,255,0.7) }' +
 	'div.swagger-ui .info .title small pre { padding: 2px 5px; }' +
 	'div.information-container.wrapper { margin: 6% 0 6% 0; padding: 15px; }' +
 	'div.swagger-ui .scheme-container { background: none; margin: 0 0 6% 0; padding: 0; box-shadow: none; }' +
 	'div.swagger-ui .scheme-container .schemes { margin: 0; padding: 15px; }' +
+	'section.swagger-ui section.models { border: 0; }' +
 	'div.swagger-ui section.models .model-container { background: rgba(255,255,255,0.6) !important; } ' +
 	'div.swagger-ui section.models .model-container:hover { background: white !important; } ' +
 	'div.box, div.swagger-ui .wrapper { overflow: visible; } ' +
